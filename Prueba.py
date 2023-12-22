4766f2c3e28350c1f49cb9f22167a1213553f7f5
sergio <alejotrujillo146@gmail.com>
2023-12-22 09:11:54 -0500

    La suma recibe 3 numeros, en vez de 2

--- Prueba.py
+++ Prueba.py
@@ -1,7 +1,8 @@
 num1=int(input("Digite un número: "))
 num2=int(input("Digite otro número: "))
+num3=int(input("Digite un tercer numero: "))
 
-def suma(num1, num2):
-    return num1 + num2
+def suma(num1, num2, num3):
+    return num1 + num2 + num3
 
-print(suma(num1 , num2))
+print(suma(num1 , num2, num3))
