6ecbd5b65b409dcb837d1cb31c13f334e6c2bc7a
sergio <alejotrujillo146@gmail.com>
2023-12-22 09:24:04 -0500

    Se reemplazo la funcion suma por division

--- Prueba.py
+++ Prueba.py
@@ -1,7 +1,7 @@
 num1=int(input("Digite un número: "))
 num2=int(input("Digite otro número: "))
 
-def suma(num1, num2):
-    return num1 + num2
+def division(num1, num2):
+    return num1 / num2
 
-print(suma(num1 , num2))
+print(division(num1 , num2))
