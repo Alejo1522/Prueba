beb55cd604cff5a37f1a1e6346eeef9250e2415a
sergio <alejotrujillo146@gmail.com>
2023-12-22 09:22:07 -0500

    Se remplazo la uncion de suma por la de multiplicar

--- Prueba.py
+++ Prueba.py
@@ -1,7 +1,7 @@
 num1=int(input("Digite un número: "))
 num2=int(input("Digite otro número: "))
 
-def suma(num1, num2):
-    return num1 + num2
+def Multiplicacion(num1, num2):
+    return num1 * num2
 
-print(suma(num1 , num2))
+print(Multiplicacion(num1 , num2))
